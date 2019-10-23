80d94b7ad5a347a37ec3fb84afc963dbd10ef9e6
Junaid Farooq <junaid@evercam.io>
2019-10-23 19:08:18 +0100

    Changed client config.

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'xviynfs2',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+    dataset: process.env.GATSBY_SANITY_DATASET || 'projects'
   }
 }
